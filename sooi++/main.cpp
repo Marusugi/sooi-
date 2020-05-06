c8e7101abd77b67f44303b6f4c118a3f1c6788a1
Marusugi <pishadon57@gmail.com>
2020-05-06 11:38:00 +0900

    Initial Commit

---
+++ sooi++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sooi++
+//
+//  Created by Marusugi on 2020/05/06.
+//  Copyright © 2020 Marusugi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
